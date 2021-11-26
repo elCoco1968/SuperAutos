662f69a42da2010f49f21bfb9952724cbc5e352e
Kevin Morales <keviinmoralees132@gmail.com>
2021-11-26 17:07:22 -0500

    prueba final

--- js/nav.js
+++ js/nav.js
@@ -2,28 +2,22 @@ const btnNav = document.getElementById('header__buttonNav');
 const nav = document.getElementById('container-nav');
 esDesktop = () => window.innerWidth <= 1024;
 
-
-
 if (!esDesktop()) {
     nav.classList.toggle('activo');
 }
-
 btnNav.addEventListener("click", (e) => {
     e.preventDefault();
     nav.classList.toggle('activo');
     subMenu.classList.remove('activo');
 
 });
-
 const btnSub = document.getElementById('nav__link-submenu');
 const subMenu = document.getElementById('submenu__items');
 const btnRegresar = document.getElementById('nav__buttonRegresar')
-
 btnSub.addEventListener("click", (e) => {
     e.preventDefault();
         subMenu.classList.add('activo');
 });
-
 btnRegresar.addEventListener("click", (e) => {
         subMenu.classList.remove('activo');
 });
